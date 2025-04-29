c1be3503a288b6c1cd660f8ed07e65444b5f7485
Shaikh Tabresh <159866113+tabresh04@users.noreply.github.com>
2025-04-29 10:50:50 +0530

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = () => {
       id: 2,
       title: 'GYM',
       category: 'web',
-      image: './assets/images/GYM.png',
+      image: './src/assets/images/GYM.png',
       tags: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://tabresh04.github.io/GYMInf/'
     },
