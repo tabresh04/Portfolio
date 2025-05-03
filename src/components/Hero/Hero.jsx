c58be48a96ae53451cb627fe780055e6e8168bcc
Shaikh Tabresh <159866113+tabresh04@users.noreply.github.com>
2025-05-03 17:20:02 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
           <p className={styles.heroText}>
             I craft digital experiences that are fast, accessible, and visually appealing.
           </p>
-          <a href="../.././public/Tabresh_Resume1.pdf">
+          <a href="https://drive.google.com/file/d/1kVuRUaz6s4UoBZSJcg1f9_C6nlDvVeqH/view?usp=drivesdk">
           <button className={styles.heroButton}>
             Download CV
             <span className={styles.buttonArrow}>→</span>
